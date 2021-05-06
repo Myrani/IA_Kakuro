c6fe609c97669b078638a4a041d28f48eef51948
myrani <myrani@protonmail.com>
2021-05-06 18:11:31 +0200

    Rajout de commentaires

--- IA_Kakuro/Grid.py
+++ IA_Kakuro/Grid.py
@@ -1,4 +1,6 @@
+### Remplissage intelligent des espaces jouables de la gille
 def gridSmartFiller(grid):
+    # Change les characters à modifier en fonction de la case actuel au besoin 
     for line in grid:
         filler ="#/#"
         for case in line:
@@ -14,12 +16,14 @@ def gridSmartFiller(grid):
                 case[0] = filler
     return grid
 
+### Générateur de Grille + Instruction pour simuler le cas du projet
 def gridMaker(x,y):
+    # génération de la grille
     grid = []
     for x in range(0,x):
         grid.append([["#/#"] for y in range(0,y)])
 
-
+    # Instructions spécifiques 
     grid[0][2][0] = "11/#"
     grid[0][3][0] = "4/#"
     grid[1][1][0] = "14/5"
@@ -33,7 +37,7 @@ def gridMaker(x,y):
     
     return gridSmartFiller(grid)
 
-
+### Print lisible pour l'homme du Kakuro dans le terminal
 def terminalPrint(kakuro):
     for line in kakuro:
         for case in line:
