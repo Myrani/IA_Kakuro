c170fa624bb35665c87b96e148138cd1cf4f17c8
myrani <myrani@protonmail.com>
2021-05-20 09:34:08 +0200

    Changement visuel minîme

--- Kakuro_Helper/Front_End/Widgets/Loader_Button_Widget.py
+++ Kakuro_Helper/Front_End/Widgets/Loader_Button_Widget.py
@@ -14,7 +14,8 @@ class LoaderButton(QtWidgets.QPushButton):
         super().__init__(parent)
 
         self.filename = filename
-        self.button = QtWidgets.QPushButton(self.filename, self)
+        self.button = QtWidgets.QPushButton(
+            self.filename.replace(".pkl", ""), self)
         self.button.pressed.connect(self.loadKakuro)
         self.button.setStyleSheet(
             "border-style: solid; background-color: rgba(34,34,34,255); border-color:black ;background: transparent;")
